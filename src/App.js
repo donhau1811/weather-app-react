3d1658a1613b5afb8fe336ec78da43a9fdca9215
donhau1811 <trandonhau1811@gmail.com>
2022-03-28 00:28:25 +0700

    hehe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const api = {
+  key: "f903705c5322278d9d8adc72ea844667",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
+  const [searchInput, setSearchInput] = useState("");
+  const [searchCity, setSearchCity] = useState("");
+  const [weatherInfo, setWeatherInfo] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      if (!searchCity) return;
+      setLoading(true);
+      //Process
+      try {
+        const url = `${api.base}weather?q=${searchCity}&units=metric&APPID=${api.key}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        if (response.ok) {
+          setWeatherInfo(
+            <div className="infor-weather">
+              <div className="city">
+                {data.name}, {data.sys.country}
+              </div>
+              <div className="description">
+                Feel like {data.main.feels_like}℃.{" "}
+                {data.weather[0].description.charAt(0).toUpperCase() +
+                  data.weather[0].description.slice(1)}{" "}
+              </div>
+              <div className="templature">
+                <img
+                  src={`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`}
+                  alt=""
+                />
+                <div>{data.main.temp}℃</div>
+              </div>
+            </div>
+          );
+          setErrorMessage("");
+        } else {
+          setErrorMessage(data.message);
+        }
+      } catch (error) {
+        setErrorMessage(error.message);
+      }
+      setLoading(false);
+    };
+    fetchWeatherData();
+  }, [searchCity]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearchCity(searchInput);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="weather-app">
+      <form onSubmit={handleSubmit}>
+        <input className='input'
+          type="text"
+          placeholder="City"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <button className="myButton">Search</button>
+      </form>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          {errorMessage ? (
+            <div style={{ color: "red" }}>{errorMessage}</div>
+          ) : (
+            <div>{weatherInfo}</div>
+          )}
+        </>
+      )}
+      </div>
   );
 }
 
